93e883e2900132c453045ee30b7958dd058e4222
Denda <denda.hanoi@gmail.com>
2019-03-07 10:50:07 +0700

    Clean code and fix url already exists

--- Model/Product/CategoryProcessor.php
+++ Model/Product/CategoryProcessor.php
@@ -1,108 +1,91 @@
 <?php
 
-
 namespace SITC\Sinchimport\Model\Product;
 
 use Magento\Catalog\Model\Category;
-use Magento\CatalogUrlRewrite\Model\Category\ChildrenCategoriesProvider;
 use Magento\CatalogUrlRewrite\Model\CategoryUrlPathGenerator;
 use Magento\CatalogUrlRewrite\Model\CategoryUrlRewriteGenerator;
-use Magento\CatalogUrlRewrite\Service\V1\StoreViewService;
 use Magento\UrlRewrite\Model\UrlPersistInterface;
 
 class CategoryProcessor
 {
-    /**
-     * Delimiter in category path.
-     */
-    const DELIMITER_CATEGORY = '/';
-    
-    /**
-     * @var CategoryUrlPathGenerator
-     */
-    protected $categoryUrlPathGenerator;
-    
-    /**
-     * @var \Magento\CatalogUrlRewrite\Model\Category\ChildrenCategoriesProvider
-     */
-    protected $childrenCategoriesProvider;
-    
-    /**
-     * @var StoreViewService
-     */
-    protected $storeViewService;
-    
-    /**
-     * @var \Magento\Catalog\Model\ResourceModel\Category\CollectionFactory
-     */
-    protected $categoryColFactory;
-    
     /**
      * Categories id to object cache.
      *
      * @var array
      */
     protected $categoriesCache = [];
-    
+
     /**
-     * Instance of catalog category factory.
-     *
+     * @var \Magento\Catalog\Model\ResourceModel\Category\CollectionFactory
+     */
+    protected $categoryColFactory;
+
+    /**
      * @var \Magento\Catalog\Model\CategoryFactory
      */
     protected $categoryFactory;
-    
+
+    /**
+     * @var CategoryUrlPathGenerator
+     */
+
+    protected $categoryUrlPathGenerator;
+
     /**
      * @var CategoryUrlRewriteGenerator
      */
     protected $categoryUrlRewriteGenerator;
-    
+
     /**
      * @var UrlPersistInterface
      */
     protected $urlPersist;
-    
+
     /**
+     * CategoryProcessor constructor.
      * @param \Magento\Catalog\Model\ResourceModel\Category\CollectionFactory $categoryColFactory
-     * @param \Magento\Catalog\Model\CategoryFactory                          $categoryFactory
+     * @param \Magento\Catalog\Model\CategoryFactory $categoryFactory
+     * @param CategoryUrlPathGenerator $categoryUrlPathGenerator
+     * @param CategoryUrlRewriteGenerator $categoryUrlRewriteGenerator
+     * @param UrlPersistInterface $urlPersist
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @throws \Magento\UrlRewrite\Model\Exception\UrlAlreadyExistsException
      */
     public function __construct(
         \Magento\Catalog\Model\ResourceModel\Category\CollectionFactory $categoryColFactory,
         \Magento\Catalog\Model\CategoryFactory $categoryFactory,
         CategoryUrlPathGenerator $categoryUrlPathGenerator,
-        ChildrenCategoriesProvider $childrenCategoriesProvider,
-        StoreViewService $storeViewService,
         CategoryUrlRewriteGenerator $categoryUrlRewriteGenerator,
         UrlPersistInterface $urlPersist
     ) {
-        $this->categoryColFactory          = $categoryColFactory;
-        $this->categoryFactory             = $categoryFactory;
-        $this->categoryUrlPathGenerator    = $categoryUrlPathGenerator;
-        $this->childrenCategoriesProvider  = $childrenCategoriesProvider;
-        $this->storeViewService            = $storeViewService;
+        $this->categoryColFactory = $categoryColFactory;
+        $this->categoryFactory = $categoryFactory;
         $this->categoryUrlRewriteGenerator = $categoryUrlRewriteGenerator;
-        $this->urlPersist                  = $urlPersist;
+        $this->categoryUrlPathGenerator = $categoryUrlPathGenerator;
+        $this->urlPersist = $urlPersist;
+
         $this->initCategories();
     }
 
     /**
      * @return $this
-     * @throws \Exception
+     * @throws \Magento\Framework\Exception\LocalizedException
      * @throws \Magento\UrlRewrite\Model\Exception\UrlAlreadyExistsException
      */
     protected function initCategories()
     {
         if (empty($this->categoriesCache)) {
-            $collection = $this->categoryColFactory->create();
-            $collection->addAttributeToSelect('name')
+            $collection = $this->categoryColFactory->create()
+                ->addAttributeToSelect('name')
                 ->addAttributeToSelect('url_key')
                 ->addAttributeToSelect('url_path')
                 ->setOrder('level');
-            
-            /* @var $collection \Magento\Catalog\Model\ResourceModel\Category\Collection */
+
+            // save category url_key and url_path
             foreach ($collection as $category) {
-                // save category url_key
                 if ($category->getId() != Category::TREE_ROOT_ID
-                    && ! in_array(
+                    && !in_array(
                         $category->getParentId(),
                         [Category::ROOT_CATEGORY_ID, Category::TREE_ROOT_ID]
                     )
@@ -111,47 +94,31 @@ class CategoryProcessor
                         $this->categoryUrlPathGenerator->getUrlKey($category)
                     );
                     $category->getResource()->saveAttribute(
-                        $category,
-                        'url_key'
+                        $category, 'url_key'
                     );
-                }
-            }
-            
-            $urlRewrites = [];
-            
-            $collection->clear()->load();
-            /* @var $collection \Magento\Catalog\Model\ResourceModel\Category\Collection */
-            foreach ($collection as $category) {
-                // save category url_path
-                if ($category->getId() != Category::TREE_ROOT_ID
-                    && ! in_array(
-                        $category->getParentId(),
-                        [Category::ROOT_CATEGORY_ID, Category::TREE_ROOT_ID]
-                    )
-                ) {
+
                     $category->setUrlPath(
                         $this->categoryUrlPathGenerator->getUrlPath($category)
                     );
                     $category->getResource()->saveAttribute(
-                        $category,
-                        'url_path'
-                    );
-                    
-                    $urlRewrites = array_merge(
-                        $urlRewrites,
-                        $this->categoryUrlRewriteGenerator->generate($category)
+                        $category, 'url_path'
                     );
                 }
-                
+
                 $this->categoriesCache[$category->getId()] = $category;
             }
-            
-            $this->urlPersist->replace($urlRewrites);
+
+            // generate category url rewrites
+            foreach ($this->categoriesCache as $category) {
+                if ($category->getLevel() == 2) {
+                    $this->urlPersist->replace($this->categoryUrlRewriteGenerator->generate($category));
+                }
+            }
         }
-        
+
         return $this;
     }
-    
+
     /**
      * Get category by Id
      *
@@ -161,7 +128,6 @@ class CategoryProcessor
      */
     public function getCategoryById($categoryId)
     {
-        return isset($this->categoriesCache[$categoryId])
-            ? $this->categoriesCache[$categoryId] : null;
+        return isset($this->categoriesCache[$categoryId]) ? $this->categoriesCache[$categoryId] : null;
     }
 }
