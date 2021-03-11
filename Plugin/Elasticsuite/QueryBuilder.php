6904147a8f784b29c369d41b293d9bd5ac799505
joseph.metcalfe <joseph.metcalfe@stockinthechannel.com>
2021-03-11 19:00:48 +0000

    Redirect to category page if search term exactly matches category name

--- Plugin/Elasticsuite/QueryBuilder.php
+++ Plugin/Elasticsuite/QueryBuilder.php
@@ -2,17 +2,46 @@
 
 namespace SITC\Sinchimport\Plugin\Elasticsuite;
 
+use Magento\Catalog\Model\ResourceModel\Category\CollectionFactory;
+use Magento\Framework\App\Response\Http as HttpResponse;
+
 
 class QueryBuilder {
 
-    public function afterCreate(\Smile\ElasticsuiteCore\Search\Request\Query\Fulltext\QueryBuilder $_subject, $result)
+    /**
+     * @var \Magento\Catalog\Model\ResourceModel\Category\CollectionFactory 
+     */
+    private $collectionFactory;
+    /**
+     * @var \Magento\Framework\App\Response\Http
+     */
+    private $response;
+
+
+    public function __construct(CollectionFactory $collectionFactory, HttpResponse $response)
+    {
+        $this->collectionFactory = $collectionFactory;
+        $this->response = $response;
+    }
+
+    public function afterCreate(\Smile\ElasticsuiteCore\Search\Request\Query\Fulltext\QueryBuilder $_subject, $result, $containerConfig, $queryText)
     {
         $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/joe_search_stuff.log');
         $logger = new \Zend\Log\Logger();
         $logger->addWriter($writer);
 
-        $logger->info(json_encode($result));
+        $categoryCollection = $this->collectionFactory->create()->addAttributeToFilter('name', $queryText);
 
+        if ($categoryCollection->getSize() > 0) {
+            $category = $categoryCollection->getFirstItem();
+            $logger->info("Collection size: " . strval($categoryCollection->getSize()));
+            $logger->info("Category redirect to: " . strval($category->getId()) . "|" . $category->getName() . " for search term: " . $queryText);
+            $logger->info("Category URL: " . $category->getUrl());
+
+            $this->response->setRedirect($category->getUrl())->sendResponse();
+            return null;
+        }
+        
         return $result;
     }
 }
