ee8505d37d19f5ec571edfabd65c8c31566ab572
joseph.metcalfe <joseph.metcalfe@stockinthechannel.com>
2021-06-23 14:36:01 +0100

    Redirect to category if query matches family name

--- Plugin/Elasticsuite/QueryBuilder.php
+++ Plugin/Elasticsuite/QueryBuilder.php
@@ -24,6 +24,8 @@ class QueryBuilder
 
 	const PRICE_REGEXP = "/(?(DEFINE)(?<price>[0-9]+(?:.[0-9]+)?)(?<cur>(?:\p{Sc}|[A-Z]{3})\s?))(?<query>.+?)\s+(?J:(?:below|under|(?:cheaper|less)\sthan)\s+(?&cur)?(?<below>(?&price))|(?:between|from)?\s*(?&cur)?(?<above>(?&price))\s*(?:and|to|-)\s*(?&cur)?(?<below>(?&price)))/u";
 
+	const FILTERABLE_OPTIONS = [ 'manufacturer', 'sinch_family' ];
+
 	/** @var CategoryRepositoryInterface */
 	private $categoryRepository;
 	/** @var HttpResponse */
@@ -39,6 +41,8 @@ class QueryBuilder
 	private $eavOptionTable;
 	private $sinchCategoriesTable;
 	private $sinchCategoriesMappingTable;
+	private $productFamilyTable;
+	private $sinchProductsTable;
 
 	/** @var Data */
 	private $helper;
@@ -76,6 +80,8 @@ class QueryBuilder
 		$this->eavOptionValueTable = $this->connection->getTableName('eav_attribute_option_value');
 		$this->sinchCategoriesTable = $this->connection->getTableName('sinch_categories');
 		$this->sinchCategoriesMappingTable = $this->connection->getTableName('sinch_categories_mapping');
+		$this->productFamilyTable = $this->connection->getTableName('sinch_family');
+		$this->sinchProductsTable = $this->connection->getTableName('sinch_products');
 		$this->helper = $helper;
 		$this->queryFactory = $queryFactory;
 		$this->customerSession = $customerSession;
@@ -115,19 +121,28 @@ class QueryBuilder
 		unset($doubleTokens[0]);
 
 		$optionFilters = [];
-		$brandName = $this->getBrandName(array_merge($queryTokens, $doubleTokens));
-		if (!empty($brandName)) {
-			//Trim query text to ensure the category matches
-			$queryText = trim(str_ireplace("{$brandName['value']}", '', $queryText));
-			$optionFilters[] = $brandName;
+		foreach (self::FILTERABLE_OPTIONS as $filterOptionCode) {
+			$optionValue = $this->getOptionAttributeValue($filterOptionCode, array_merge($queryTokens, $doubleTokens));
+			if (!empty($optionValue)) {
+				if ($optionValue['attribute_code'] != 'sinch_family')
+					$queryText = trim(str_ireplace("{$optionValue['value']}", '', $queryText));
+				$optionFilters[] = $optionValue;
+			}
 		}
 
-		$familyName = $this->getProductFamily(array_merge($queryTokens, $doubleTokens));
-		if (!empty($familyName)) {
-			//Trim query text to ensure the category matches
-			$queryText = trim(str_ireplace("{$familyName['value']}", '', $queryText));
-			$optionFilters[] = $familyName;
-		}
+//		$brandName = $this->getBrandName(array_merge($queryTokens, $doubleTokens));
+//		if (!empty($brandName)) {
+//			//Trim query text to ensure the category matches
+//			$queryText = trim(str_ireplace("{$brandName['value']}", '', $queryText));
+//			$optionFilters[] = $brandName;
+//		}
+//
+//		$familyName = $this->getProductFamily(array_merge($queryTokens, $doubleTokens));
+//		if (!empty($familyName)) {
+//			//Trim query text to ensure the category matches
+//			$queryText = trim(str_ireplace("{$familyName['value']}", '', $queryText));
+//			$optionFilters[] = $familyName;
+//		}
 
 		if (str_ends_with($queryText, 's')) {
 			$pluralQueryText = substr($queryText, 0, -1);
@@ -149,7 +164,7 @@ class QueryBuilder
 		$this->logger->info($queryVariants);
 
 		//If category match is successful return early
-		if ($this->checkCategoryMatch($containerConfig, $queryVariants, $priceFilter, $optionFilters)) {
+		if ($this->checkCategoryOrFamilyMatch($containerConfig, $queryVariants, $priceFilter, $optionFilters)) {
 			return null;
 		}
 
@@ -229,10 +244,10 @@ class QueryBuilder
 	 * @param ContainerConfigurationInterface $containerConfig
 	 * @param string[] $queries
 	 * @param $priceFilter
-	 * @param $brandFilter
+	 * @param array $optionFilters
 	 * @return bool true if matched
 	 */
-	private function checkCategoryMatch(ContainerConfigurationInterface $containerConfig, array $queries, $priceFilter, array $optionFilters): bool
+	private function checkCategoryOrFamilyMatch(ContainerConfigurationInterface $containerConfig, array $queries, $priceFilter, array $optionFilters): bool
 	{
 		$start = microtime(true);
 
@@ -248,6 +263,17 @@ class QueryBuilder
                 WHERE ccev.value IN ($inClause) OR sc.VirtualCategory IN ($inClause)",
 			array_merge($queries, $queries)
 		);
+
+		//Product family match if no category match detected
+		if (empty($catId)) {
+			$catId = $this->connection->fetchOne(
+				"SELECT cce.entity_id FROM {$this->categoryTable} cce
+				JOIN {$this->sinchProductsTable} sp ON sp.store_category_id = cce.store_category_id
+				JOIN {$this->productFamilyTable} pf ON pf.id = sp.family_id
+				WHERE pf.name IN ({$inClause})",
+			$queries);
+			$this->logger->info("Product family match cat ID: {$catId}");
+		}
 
 		//Category or virtual category name match, don't bother creating the ES query and instead redirect
 		if (!empty($catId)) {
