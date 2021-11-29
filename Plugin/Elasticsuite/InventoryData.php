9ab0fe1532d595c5167da634bb1f9449fa873345
joseph.metcalfe <joseph.metcalfe@stockinthechannel.com>
2021-11-29 15:00:38 +0000

    Addition of in stock filter

--- Plugin/Elasticsuite/InventoryData.php
+++ Plugin/Elasticsuite/InventoryData.php
@@ -5,9 +5,9 @@ namespace SITC\Sinchimport\Plugin\Elasticsuite;
 
 use Exception;
 use Magento\Catalog\Api\ProductRepositoryInterface;
-use Magento\Catalog\Model\Product;
-use Magento\Catalog\Model\Product\Action as ProductAction;
-use Magento\Store\Model\Store;
+use Magento\Eav\Model\Attribute;
+use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\DB\Adapter\AdapterInterface;
 use Magento\Store\Model\StoreManagerInterface;
 use SITC\Sinchimport\Logger\Logger;
 
@@ -21,39 +21,29 @@ class InventoryData
 {
     const IN_STOCK_FILTER_CODE = 'sinch_in_stock';
 
-    /**
-     * @var ProductRepositoryInterface
-     */
-    private  $productRepository;
+    private Logger $logger;
+    private StoreManagerInterface $storeManager;
+    private AdapterInterface $connection;
 
-    /**
-     * @var Logger
-     */
-    private $logger;
+    private string $catalog_product_entity_int;
+    private int $attrId;
 
-    /**
-     * @var StoreManagerInterface
-     */
-    private $storeManager;
-
-    /**
-     * InventoryData constructor.
-     * @param ProductRepositoryInterface $productRepository
-     * @param Logger $logger
-     * @param StoreManagerInterface $storeManager
-     */
     public function __construct(
-        ProductRepositoryInterface $productRepository,
         Logger $logger,
-        StoreManagerInterface $storeManager
+        StoreManagerInterface $storeManager,
+        ResourceConnection $resourceConnection,
+        Attribute $eavAttribute
     ){
-        $this->productRepository = $productRepository;
         $this->logger = $logger;
         $this->storeManager = $storeManager;
+        $this->connection = $resourceConnection->getConnection();
+
+        $this->catalog_product_entity_int = $this->connection->getTableName('catalog_product_entity_int');
+        $this->attrId = $eavAttribute->getIdByCode('catalog_product', self::IN_STOCK_FILTER_CODE);
     }
 
     /**
-     * Add stock status to product data so we can filter on it
+     * Add stock status to product data, so we can filter on it
      *
      * @param \Smile\ElasticsuiteCatalog\Model\Product\Indexer\Fulltext\Datasource\InventoryData $subject
      * @param $result
@@ -61,21 +51,15 @@ class InventoryData
     public function afterAddData(\Smile\ElasticsuiteCatalog\Model\Product\Indexer\Fulltext\Datasource\InventoryData $subject, $result)
     {
         $count = 0;
-        if (empty($result)) {
-            $this->logger->info("Result is empty");
-        }
-        foreach ($result as $key => $value) {
-            $prodId = $key;
-            try {
-                /** @var Product $product */
-                $product = $this->productRepository->getById($prodId);
-                $isInStock = $value['is_in_stock'];
-                $product->setData(self::IN_STOCK_FILTER_CODE, $isInStock);
-                $this->productRepository->save($product);
-                $count++;
-            } catch (Exception $e) {
-                $this->logger->info("Couldn't find product with ID $prodId");
-            }
+        $storeId = $this->storeManager->getDefaultStoreView()->getId();
+        foreach ($result as $prodId => $indexData) {
+            $isInStock = $indexData['stock']['is_in_stock'] ?? 0;
+            $this->connection->query("
+                INSERT INTO {$this->catalog_product_entity_int} (attribute_id, store_id, entity_id, value)
+                VALUES (:attributeId, :storeId, :entityId, :value)",
+                [':attributeId' => $this->attrId, ':storeId' => $storeId, ':entityId' => $prodId, ':value' => (int)$isInStock]
+            );
+            $count++;
         }
 
         $this->logger->info("Processed stock filter for $count products");
